78feafdb3709bab55b31165613cec9825676cc3a
JoeyYoung <1761889730@qq.com>
2018-06-27 16:06:07 +0800

    add databases

---
+++ src/main/java/tss/entities/TypeEntity.java
@@ -0,0 +1,11 @@
+package tss.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "type")
+public class Type {
+    private Long id;
+    private 
+}
